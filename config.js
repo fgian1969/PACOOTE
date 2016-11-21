fe92097632ad08bf9cd1f21114563055a4ec6449
Gian Carlo Ferrari <fgian1969@gmail.com>
2016-11-21 21:46:45 +0100

    Inseriti css e js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 xports.config = {
-  "appName": 'DELIVERY',	
+  "appName": 'PACOOTE',	
   "debug" : false,
   "hosts": ['localhost:8091'],
   "user": 'Administrator',
   "password": 'elena2000',
-  "bucket" : 'passport'
+  "bucket" : 'PACOOTE'
 };
