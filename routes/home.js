7601bb524901119044dd06206b6f50da3726e693
Ferrari Gian Carlo <fgian1969@gmsail.com>
2016-11-28 14:04:37 +0100

    Cart sistemato

--- routes/home.js
+++ routes/home.js
@@ -17,61 +17,69 @@ var carrello=[];
   output8 = loremIpsum();
   
 var ipso={
-     ipso1:output1,
-     ipso2:output2,
-     ipso3:output3,
-     ipso4:output4,
-     ipso5:output5,
-     ipso6:output6,
-     ipso7:output7,
-     ipso8:output8,
      ipso1:{
        title:"Un tappeto di boschi selvaggi",
+       img:"/images/boschi.jpg",
        code: "code1",
        desc:output1,
-       price: "€ 14,72"
+       price: "€ 14,72",
+       qta:0
      },
      ipso2:{
        title:"The Boss",
+       img:"/images/boss.jpg",
        code: "code2",
        desc:output1,
-       price: "€ 24,32"
+       price: "€ 24,32",
+       qta:0
      },
      ipso3:{
        title:"Lo spazio fra le nuvole",
+       img:"/images/camilla.jpg",
        code: "code3",
        desc:output1,
-       price: "€ 12,28"
+       price: "€ 12,28",
+       qta:0
      },
      ipso4:{
        title:"Carlo Ancelotti",
+       img:"/images/celotti.jpg",
        code: "code4",
        desc:output1,
-       price: "€ 34,22"
+       price: "€ 34,22",
+       qta:0
      },
      ipso5:{
        title:"Free States of Jones",
+       img:"/images/jones.jpg",
        code: "code5",
        desc:output1,
-       price: "€ 22,12"
+       price: "€ 22,12",
+       qta:0
      },
      ipso6:{
        title:"Lacrime",
+       img:"/images/lacrime.jpg",
        code: "code6",
        desc:output1,
-       price: "€ 11,13"
+       price: "€ 11,13",
+       qta:0
      },
      ipso7:{
        title:"Nannini",
+       img:"/images/nannini.jpg",
        code: "code7",
        desc:output1,
-       price: "€ 23,43"
+       price: "€ 23,43",
+       qta:0
      },
      ipso8:{
        title:"Una pecora nera al potere",
+       img:"/images/pecora.jpg",
        code: "code8",
        desc:output1,
-       price: "€ 44,12"
+       price: "€ 44,12",
+       qta:0
      }
      
 }
@@ -82,15 +90,16 @@ var ipso={
 
 function checkCart(req)
 {
-    //var gino=cookieParser.signedCookie("Pacoote", config.secret);
-    console.log(req.cookies.Pacoote);
-    if (req.cookies.code1) {
-      carrello=cookieParser.JSONCookie('Pacoote');
-      console.log(carrello);
-      cartItems=carrello.length;
+  cartItems=0;
+  if (req.cookies.Pacoote)
+  {
+    ipso=req.cookies.Pacoote;
+    for (var prop in ipso) {
+        cartItems+=ipso[prop].qta;
     }
-    var myobjectCart={ipso,cartItems};
-    return myobjectCart;
+  }
+  var myobjectCart={ipso,cartItems};
+  return myobjectCart;
 }
 router.get('/home', function(req, res) {
      var objectCart=checkCart(req);
@@ -104,20 +113,32 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res){
-// console.log(req.body.bookCode)
+ 
   var day = 24 * 60 * 60 * 1000;
 
-  carrello.push(req.body.code1)
-  if (req.body.code1) res.cookie('Pacoote', carrello, { maxAge: day });
-  res.redirect('back');
+  //carrello.push(req.body.bookCode)
+  if (req.body.bookCode)
+  {
+    if (req.cookies.Pacoote)
+    {
+      ipso=req.cookies.Pacoote;
+      for (var prop in req.cookies.Pacoote) {
+        if (ipso[prop].code==req.body.bookCode)
+        {
+          ipso[prop].qta+=1;
+        } 
+      }  
+    }
+  }
+   res.cookie('Pacoote', ipso, { maxAge: day });
+   res.redirect('back');
 });
 
 router.post('/home', function(req, res){
-  //  console.log(req.body.bookCode)
-  var day = 24 * 60 * 60 * 1000;
+   var day = 24 * 60 * 60 * 1000;
 
-  carrello.push(req.body.code1)
-  if (req.body.code1) res.cookie('Pacoote', carrello, { maxAge: day });
+  //carrello.push(req.body.bookCode)
+  if (req.body.bookCode) res.cookie('Pacoote', carrello, { maxAge: day });
   res.redirect('back');
 });
 
