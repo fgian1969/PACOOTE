b4d5d472848a2f8fa6cc872b9a03c7e1f41ef477
Ferrari Gian Carlo <fgian1969@gmsail.com>
2016-11-30 08:57:07 +0100

    Carrello terminato (ad,delete,sub)

--- routes/cart.js
+++ routes/cart.js
@@ -20,19 +20,47 @@ router.get('/cart', function(req, res, next) {
   }
 });
 
-router.post('/cart/set', function(req, res, next) {
-   if (req.cookies.Pacoote!=undefined)
-  {
-    cartObject.checkCart(req,function(err,ipso){
-       res.render('cart', {result:ipso.data});
-    });
-  }
-  else
-  {
-     var ipso=new cartObject();
-     res.render('cart', {result:ipso.data});
-  }
-});
+router.post('/cart', function(req, res, next) {
+  var day = 24 * 60 * 60 * 1000;
+    if (req.cookies.Pacoote)
+    {
+      console.log("Esiste cookie!");
+      ipso=req.cookies.Pacoote;
+    }
+    else
+    {
+       console.log("Non Esiste cookie!");
+      var ipso=new cartObject();
+    }
+     for (var prop in ipso.data.books)
+     {
+        if (ipso.data.books[prop].code==req.body.code) 
+        {
+           console.log("Trovato codice");
+           if (req.body.add)
+              {
+                console.log("Aumentato di 1");
+                ipso.data.books[prop].qta+=1;
+              }
+            else if (req.body.add)
+            {
+              if (ipso.data.books[prop].qta>0)
+              {
+              console.log("Diminuito di 1");
+              ipso.data.books[prop].qta-=1;
+              }
+            }
+            else
+            {
+              console.log("Articolo Eliminato");
+              ipso.data.books[prop].qta=0;
+            }
+        } 
+     }
+      res.cookie('Pacoote', ipso, { maxAge: day });
+   res.redirect('back');
+  });
+ 
 
 
 module.exports = router;
